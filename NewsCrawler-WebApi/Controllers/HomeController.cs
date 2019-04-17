482254756404575474b407f524dddc653ebc39f5
Minh Duong <minhduongnhat@outlook.com>
2019-04-17 22:35:52 +0700

    temp

--- NewsCrawler-WebApi/Controllers/HomeController.cs
+++ NewsCrawler-WebApi/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using NewsCrawler_WebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,8 +15,33 @@ namespace NewsCrawler_WebApi.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
+            CallToApi();
             return View();
+        }
+
+        private void CallToApi()
+        {
+            var path = "everything?q=bitcoin&from=2019-03-17&sortBy=publishedAt&apiKey=18ca97d02834421fa4b7429131386d08";
+            var temp = GetProductAsync(path);
+        }
+
+        static async Task<IEnumerable<JsonResult>> GetProductAsync(string path)
+        {
+            IEnumerable<JsonResult> dataObjects = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://newsapi.org/v2/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(path);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    dataObjects = response.Content.ReadAsAsync<IEnumerable<JsonResult>>().Result;
+                }
+                return await Task.FromResult(dataObjects);
+            }
         }
     }
 }
---
+++ NewsCrawler-WebApi/Models/Article.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsCrawler_WebApi.Models
+{
+    public class Article
+    {
+        public Dictionary<string, string> Source { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime? PublishedAt { get; set; }
+        public string Content { get; set; }
+    }
+}
